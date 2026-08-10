0a4cd278c3187f3abfc8d5185defa279a7510fe0
dhhwang <dohyun033@gmail.com>
2026-08-10 11:32:04 +0900

    chore: vite base 경로 끝 슬래시 제거

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
-  base: "/react-netflix-app/",
+  base: "/react-netflix-app",
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
